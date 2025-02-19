a92e5c39752e7143f9eec4079a3438050e16123a
techniquecole <79592552+techniquecole@users.noreply.github.com>
2025-02-19 02:48:04 -0500

    Update wygomp.js

--- wygomp.js
+++ wygomp.js
@@ -28,7 +28,6 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('my-cache').then(cache => {
             return cache.addAll([
-                '/',
                 '/index.html',
                 '/styles.css',
                 '/script.js',
